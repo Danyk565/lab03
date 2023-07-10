c30f3e1cfd7dbaa9fe1ef544dccd354bf91f166e
Daniil <abdushaev04@list.ru>
2023-07-10 13:46:57 +0300

    code: Добавление Headr

--- Header.h
+++ Header.h
@@ -1,1 +1,12 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+void find_minmax(const vector<double>& numbers, double& min, double& max);
+void svg_begin(double width, double height);
+void svg_text(double left, double baseline, string text);
+void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black");
+void svg_end();
+void show_histogram_svg(const vector<size_t>bins, int BLOCK_WIDTH);
