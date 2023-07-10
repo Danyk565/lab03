ae01cee75491e9a312243ac68a66b6120417a7fb
Daniil <abdushaev04@list.ru>
2023-07-10 13:27:24 +0300

    code: Добавление SVG

--- lab003-histogram.cpp
+++ lab003-histogram.cpp
@@ -5,6 +5,58 @@
 #include<string>
 using namespace std;
 
+void svg_begin(double width, double height) {
+	cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+	cout << "<svg ";
+	cout << "width='" << width << "' ";
+	cout << "height='" << height << "' ";
+	cout << "viewBox='0 0 " << width << " " << height << "' ";
+	cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+void svg_text(double left, double baseline, string text) {
+	cout << "<text x='" << left << "' y='" << baseline << "'> " << text << "</text>" << '\n';
+}
+
+void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black") {
+	cout << "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "' stroke='red' fill='#ffeeee'/>" << '\n';
+}
+
+void svg_end() {
+	cout << "</svg>\n";
+}
+void show_histogram_svg(const vector<size_t>bins, int BLOCK_WIDTH) {
+	const auto IMAGE_WIDTH = 800;
+	const auto IMAGE_HEIGHT = 600;
+	const auto TEXT_LEFT = 20;
+	const auto TEXT_BASELINE = 20;
+	const auto TEXT_WIDTH = 50;
+	const auto BIN_HEIGHT = 30;
+	//const auto BLOCK_WIDTH = 20;
+
+	svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+	double top = 0;
+	for (size_t bin : bins) {
+		const double bin_width = BLOCK_WIDTH * bin;
+		svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+		svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
+		top += BIN_HEIGHT;
+	}
+	svg_end();
+}
+void find_minmax(const vector<double>& numbers, double& min, double& max) {
+	if (numbers.size() == 0)
+		return;
+	min = numbers[0];
+	max = numbers[0];
+	for (double x : numbers) {
+		if (x < min) {
+			min = x;
+		}
+		else if (x > max) {
+			max = x;
+		}
+	}
+}
 
 void arr_sorting(vector<size_t> arr) {
 	double max = *max_element(arr.begin(), arr.end());
